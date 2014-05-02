add6ea1100f2cba867db8ea3d7b9c8053181a067
kaleb-himes <kaleb.himes@gmail.com>
2014-05-01 19:18:22 -0600

    readability

--- Swap/Swap.c
+++ Swap/Swap.c
@@ -4,14 +4,23 @@ int main(void){
     int a, b;
     a = 5;
     b = 10;
+
     //print prior
     printf("Prior to swap:\n");
     printf("a = %d, b = %d\n\n", a, b);
-    //swap
+
+
+    //begin swap
+
     a = (a + b) - (b = a);
+
+    //end swap
+
+
     //print after
     printf("After swap:\n");
     printf("a = %d, b = %d\n\n", a, b);
+
     //pause before termination of program.
     char c;
     printf("Enter any letter and hit return to exit");
